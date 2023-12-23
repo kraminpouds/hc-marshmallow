3837339ac722507fa93349583258d4fd5e7588d1
Krad Zhao <Xin.Zhao@PerkinElmer.com>
2023-12-23 22:07:31 +0800

    admin - box/letter 1 list letters in the selected box 2 split letters read or unread 3 set remark for letter 4 toggle read/unread

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -1,6 +1,128 @@
+import { Badge, Input, Layout, List, Menu, MenuProps, Modal, Segmented, theme } from 'antd';
+import axios from 'axios';
+import * as dayjs from 'dayjs'
+import { useEffect, useState } from 'react';
+import { Box, Letter } from './model';
+
 function Admin() {
+    const [box, setBox] = useState<string>('');
+    const [boxes, setBoxes] = useState<Box[]>([]);
+    const [items, setItems] = useState<MenuProps.items>([]);
+    const [letters, setLetters] = useState<Letter[]>([]);
+    const [unreadItems, setUnreadItems] = useState<Letter[]>([]);
+    const [readItems, setReadItems] = useState<Letter[]>([]);
+    const [isRead, setIsRead] = useState<boolean>(false);
+    const [remarkLetter, setRemarkLetter] = useState<Letter>();
+    const [remark, setRemark] = useState<string>('');
+
+    const { token: { colorBgContainer, borderRadiusLG } } = theme.useToken();
+
+    useEffect(() => {
+        axios.get('/api/box/all').then(r => setBoxes(r.data));
+    }, []);
+
+    useEffect(() => {
+        setItems(boxes.map(d=>({
+            key: d.uuid,
+            label: d.name
+        })));
+    }, [boxes]);
+
+    useEffect(() => {
+        if(box) {
+            axios.get(`/api/letter/${box}`).then(r => setLetters(r.data));
+        } else {
+            setLetters([]);
+        }
+    }, [box]);
+
+    useEffect(() => {
+        setUnreadItems(letters.filter(l=>!l.isRead).sort((x,y)=>y.createDate-x.createDate));
+        setReadItems(letters.filter(l=>l.isRead).sort((x,y)=>y.createDate-x.createDate));
+    }, [letters]);
+
+    useEffect(() => {
+        setRemark(remarkLetter?.remark??'');
+    }, [remarkLetter]);
+
+    const toggleReadState = (letter: Letter) => {
+        const newLetter = {
+            ...letter,
+            isRead: !letter.isRead
+        };
+        axios.patch(`/api/letter`, newLetter).then(() => {
+            setLetters([...letters.filter(l=>l!==letter), newLetter]);
+        });
+    }
+
+    const updateRemark = () => {
+        if(!remarkLetter) return;
+        const newLetter = {
+            ...remarkLetter,
+            remark: remark
+        };
+        axios.patch(`/api/letter`, newLetter).then(() => {
+            setLetters([...letters.filter(l=>l!==remarkLetter), newLetter]);
+            setRemarkLetter(undefined);
+        });
+    }
+
     return (
-        <div>Hello world Admin</div>
+        <Layout style={{ minHeight: '100vh' }}>
+            <Layout.Header className={'title-admin'}>
+                狐乱生草の棉花糖 - 后台
+            </Layout.Header>
+            <Layout>
+                <Layout.Sider>
+                    <Menu mode="inline"
+                          defaultSelectedKeys={[items[0]?.key]}
+                          items={items}
+                          style={{ height: '100%', borderRight: 0 }}
+                          onClick={(e)=>setBox(e.key)} />
+                </Layout.Sider>
+                <Layout style={{ padding: '0 24px 24px' }}>
+                    <Layout.Content style={{
+                        padding: 24,
+                        margin: 0,
+                        minHeight: 280,
+                        background: colorBgContainer,
+                        borderRadius: borderRadiusLG,
+                    }}>
+                        <Badge count={unreadItems.length}>
+                            <Segmented options={[
+                                { label: '已读', value: true},
+                                { label: '未读', value: false},
+                            ]} value={isRead} onChange={setIsRead} style={{ marginBottom: '8px'}} />
+                        </Badge>
+                        <List bordered
+                              pagination={!!letters.length}
+                              dataSource={isRead?readItems:unreadItems}
+                              itemLayout='vertical'
+                              renderItem={(item) => (
+                            <List.Item key={item.uuid} actions={[
+                                (<a onClick={()=>setRemarkLetter(item)} style={{marginTop: '8px'}}>备注</a>),
+                                (<a onClick={()=>toggleReadState(item)}>
+                                    {isRead?'标记未读':'标记已读'}
+                                </a>),
+                            ]}>
+                                <List.Item.Meta title={dayjs(item.createDate).format('YYYY年M月D日 H:mm:ss')} description={item.remark} />
+                                {item.content}
+                            </List.Item>
+                        )} />
+                        <Modal
+                            title="备注"
+                            centered
+                            style={{ top: 20 }}
+                            open={!!remarkLetter}
+                            onOk={() => updateRemark()}
+                            onCancel={() => setRemarkLetter(undefined)}
+                        >
+                            <Input value={remark} onChange={(e)=>setRemark(e.target.value)} />
+                        </Modal>
+                    </Layout.Content>
+                </Layout>
+            </Layout>
+        </Layout>
     )
 }
 
--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,19 @@
 import { Router } from "express";
 import { JsonDB, Config } from 'node-json-db';
 import { v4 } from 'uuid';
-import { Letter } from './model';
+import { Box, Letter } from './model';
 
-const db = new JsonDB(new Config("db", true, false, '/'));
+const db = new JsonDB(new Config("db", true, true, '/'));
 await db.reload();
 
 const router = Router();
 
 router.get('/api/box', async (req, res) => {
+    const data = await db.getObjectDefault("/boxes", []);
+    res.status(200).end(JSON.stringify(data.filter(d=>d.enabled)));
+});
+
+router.get('/api/box/all', async (req, res) => {
     const data = await db.getObjectDefault("/boxes", []);
     res.status(200).end(JSON.stringify(data));
 });
@@ -21,10 +26,11 @@ router.put('/api/box', async (req, res) => {
 
 router.patch('/api/box', async (req, res) => {
     try {
-        if (req.body.uuid) {
-            const index = await db.getIndex("/boxes", req.body.id, 'uuid');
+        const box = req.body as Box;
+        if (box.uuid) {
+            const index = await db.getIndex("/boxes", box.uuid, 'uuid');
             if(index>=0) {
-                await db.push(`/boxes[${index}]`, req.body, true);
+                await db.push(`/boxes[${index}]`, box, true);
             }
             res.status(200).end();
         } else {
@@ -67,11 +73,42 @@ router.post('/api/box/disable', async (req, res) => {
     }
 });
 
+router.get('/api/letter/:boxUUID', async (req, res) => {
+    try {
+        console.log('/api/letter', req.params.boxUUID);
+        if (req.params.boxUUID) {
+            const data = await db.getObjectDefault(`/letters/${req.params.boxUUID}`, []);
+            res.status(200).end(JSON.stringify(data));
+        } else {
+            res.status(200).end(JSON.stringify([]));
+        }
+    } catch (e) {
+        res.status(500).end(e);
+    }
+});
+
 router.put('/api/letter', async (req, res) => {
     try {
         const letter = req.body as Letter;
-        await db.push(`/letters/${letter.boxUUID}[]`, { ...letter, uuid: v4(), isRead: false, remark: '' }, true);
+        await db.push(`/letters/${letter.boxUUID}[]`, { ...letter, uuid: v4(), isRead: false, remark: '', createDate: Date.now().valueOf() }, true);
         res.status(200).end();
+    } catch (e) {
+        res.status(500).end(e);
+    }
+});
+
+router.patch('/api/letter', async (req, res) => {
+    try {
+        const letter = req.body as Letter;
+        if (letter.boxUUID && letter.uuid) {
+            const index = await db.getIndex(`/letters/${letter.boxUUID}`, letter.uuid, 'uuid');
+            if(index>=0) {
+                await db.push(`/letters/${letter.boxUUID}[${index}]`, letter, true);
+            }
+            res.status(200).end();
+        } else {
+            res.status(400).end();
+        }
     } catch (e) {
         res.status(500).end(e);
     }
